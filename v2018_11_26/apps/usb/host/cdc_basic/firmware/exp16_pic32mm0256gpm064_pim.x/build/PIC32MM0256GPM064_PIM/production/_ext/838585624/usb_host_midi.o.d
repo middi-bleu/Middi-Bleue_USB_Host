f0f03a4f7194821baf9386c5f6b2073f75d9feac
Stentsoe <48253311+Stentsoe@users.noreply.github.com>
2019-04-01 16:13:10 +0200

    ignore and midi_host.h add

---
+++ v2018_11_26/apps/usb/host/cdc_basic/firmware/exp16_pic32mm0256gpm064_pim.x/build/PIC32MM0256GPM064_PIM/production/_ext/838585624/usb_host_midi.o.d
@@ -0,0 +1,10 @@
+build/PIC32MM0256GPM064_PIM/production/_ext/838585624/usb_host_midi.o:  \
+ ../../../../../../framework/usb/src/usb_host_midi.c  \
+ ../../../../../../framework/usb/inc/usb.h  \
+../demo_src/usb_config.h  \
+ ../../../../../../framework/usb/inc/usb_common.h  \
+ ../../../../../../framework/usb/inc/usb_ch9.h  \
+ ../../../../../../framework/usb/inc/usb_host.h  \
+ ../../../../../../framework/usb/inc/usb_hal.h  \
+ ../../../../../../framework/usb/inc/usb_hal_pic32mm.h  \
+ ../../../../../../framework/usb/inc/usb_host_midi.h 
